2eaa635a2a0b25a4953b51ea7dc2898c629096c1
Kuvuklija <cerakuzz@mail.ru>
2019-04-17 21:57:19 +0300

    api (begin)

---
+++ chapter_9.js/js/geolocation.js
@@ -0,0 +1,26 @@
+﻿// В браузере Chrome в автономном режиме данный пример не работает (но функционирует на веб-сервере).
+// В других браузерах может появиться запрос о получении разрешения на определение местоположения.
+// Процесс определения местоположения занимает некоторое время.
+
+var elMap=document.getElementById('loc');
+var msgError='Sorry, we were unable to get your location';
+
+if(Modernizr.geolocation){
+  navigator.geolocation.getCurrentPosition(success, fail);
+  elMap.textContent='Get position...';
+}else{
+  elMap.textContent=msgError;
+}
+
+function success(position){
+  msgLoc='<h3>Longitude:<br>';
+  msgLoc+=position.coords.longitude+'</h3>';
+  msgLoc+='<h3>Latitude:<br>';
+  msgLoc+=position.coords.latitude+'</h3>';
+  elMap.innerHTML=msgLoc;
+}
+
+function fail(msg){
+  elMap.textContent=msg;
+  console.log(msg.code+' '+msg.message+ ': see page 424 in the book');
+}
---
+++ chapter_9.js/js/local-storage.js
@@ -0,0 +1,16 @@
+﻿if(window.localStorage){
+
+  var elUserName=document.getElementById('username');
+  var elAnswer=document.getElementById('answer');
+
+  elUserName.value=localStorage.getItem('username');
+  elAnswer.value=localSrorage.getItem('answer');
+
+  elUserName.addEventListener('input', function(){
+      localStorage.setItem('username', elUserName.value);
+  });
+
+  elAnswer.addEventListener('input', function(){
+      localStorage.setItem('answer', elAnswer.value);
+  });
+}
